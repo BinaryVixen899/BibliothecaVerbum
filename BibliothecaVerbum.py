c93455f505a16dabd1e1b72c257d1dbdd31f7c48
BinaryVixen899 <lealac@msn.com>
2021-08-05 07:18:29 -0700

    Working basic version

--- BibliothecaVerbum.py
+++ BibliothecaVerbum.py
@@ -9,17 +9,25 @@ assert opts.headless == True
 browser = Chrome(options=opts)
 browser.get('https://www.goodreads.com/quotes/tag/harry-potter?page=1')
 results = browser.find_elements_by_class_name('quoteText')
-for x in results:
-    print (x.text + "\n")
+
 while True:
     nextbutton = browser.find_element_by_class_name("next_page")
     nextbutton.click()
     results = browser.find_elements_by_class_name('quoteText')
-    for x in results:
-        print (x.text + "\n")
     try:
       browser.find_element_by_class_name("next_page disabled")
       False
     except:
       True
+    for x in results:
+        if 'J.K. Rowling' in x.text:
+            print (x.text + "\n")
+            try:
+              browser.find_element_by_class_name("next_page disabled")
+              break
+              False
+            except:
+              True
+            
+    
 browser.quit()
